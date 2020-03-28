7ceb789f7c2481afbdbbced10bed761a5cdaeff9
Ignacio De La Madrid <ignacio.delamadrid@espressive.com>
2020-03-28 12:20:40 -0600

    Basic countdown endpoint

--- src/app.py
+++ src/app.py
@@ -4,8 +4,30 @@ from random import randint
 
 from flask import Flask, render_template
 
-from flask_ask import Ask, statement, question, session
+from flask_ask import Ask, audio, statement, question, session
 
+NUMBERS = {
+    1: "uno",
+    2: "dos",
+    3: "tres",
+    4: "cuatro",
+    5: "cinco",
+    6: "seis",
+    7: "siete",
+    8: "ocho",
+    9: "nueve",
+    10: "diez",
+    11: "once",
+    12: "doce",
+    13: "trece",
+    14: "catorce",
+    15: "quince",
+    16: "dieciseis",
+    17: "diecisiete",
+    18: "dieciocho",
+    19: "diecinueve",
+    20: "veinte",
+}
 
 app = Flask(__name__)
 
@@ -20,23 +42,15 @@ def new_game():
     return question(welcome_msg)
 
 
-@ask.intent("YesIntent")
-def next_round():
-    numbers = [randint(0, 9) for _ in range(3)]
-    round_msg = render_template("round", numbers=numbers)
-    session.attributes["numbers"] = numbers[::-1]  # reverse
-    return question(round_msg)
+@ask.intent("OneshotTakeoffIntent", convert={"seconds": int})
+def answer(seconds):
+    seconds = seconds or 10
+    count = range(1, seconds + 1)
+    words = reversed(list(map(lambda num: NUMBERS[num], count)))
 
-
-@ask.intent("AnswerIntent", convert={"first": int, "second": int, "third": int})
-def answer(first, second, third):
-    winning_numbers = session.attributes["numbers"]
-    if [first, second, third] == winning_numbers:
-        msg = render_template("win")
-    else:
-        msg = render_template("lose")
-
-    return statement(msg)
+    speech = render_template("takeoff", words=words)
+    stream_url = 'https://freesound.org/data/previews/93/93078_1386366-lq.mp3'
+    return audio(speech).play(stream_url)
 
 
 if __name__ == "__main__":
